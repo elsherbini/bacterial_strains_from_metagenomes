641856c45dd7c2adf20b70ed28e785e87891244c
Elsherbini, Joseph Ahmed <jelsherbini@mgh.harvard.edu>
2025-05-24 13:29:17 -0400

    add cluster submission script

---
+++ submit_jobs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=5-00:00:00
+#SBATCH --mem=16GB
+#SBATCH --partition=priority
+#SBATCH --job-name=submit_jobs
+
+
+snakemake  --executor slurm --default-resources slurm_partition=short --use-conda --jobname {rulename}.{jobid} --jobs 2000 --keep-going --rerun-incomplete
